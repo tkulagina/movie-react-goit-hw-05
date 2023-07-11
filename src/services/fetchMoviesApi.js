a5e56521d33e25b757e1ae285b824c95dd1594b7
tkulagina <tkulagina1974@gmail.com>
2023-07-11 15:52:40 +0300

    update

---
+++ src/services/fetchMoviesApi.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const KEY = '0101a20a1be742d85169ae7a078f6128';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+
+export const getTrandingMovies = async () => {
+  const response = await axios.get(
+    `${BASE_URL}trending/movie/day?api_key=${KEY}&language=en-US`
+  );
+  return response.data;
+};
+
+export const getMoviesQuery = async query => {
+  const response = await axios.get(
+    `${BASE_URL}search/movie?api_key=${KEY}&query=${query}&language=en-US`
+  );
+  return response.data;
+};
+
+export const getMovieById = async id => {
+  const response = await axios.get(
+    `${BASE_URL}movie/${id}?api_key=${KEY}&language=en-US`
+  );
+  return response.data;
+};
+
+export const getMovieCast = async id => {
+  const response = await axios.get(
+    `${BASE_URL}movie/${id}/credits?api_key=${KEY}&language=en-US`
+  );
+  return response.data.cast;
+};
+
+export const getMovieReviews = async id => {
+  const response = await axios.get(
+    `${BASE_URL}movie/${id}/reviews?api_key=${KEY}&language=en-US&page=1`
+  );
+  return response.data.results;
+};
